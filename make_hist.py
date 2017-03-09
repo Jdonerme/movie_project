7de7df58c12ec156c41b01110ff3d18385bc8e09
Malavika Venkatesan <malavika1996@hotmail.com>
2017-03-08 17:34:39 -0800

    renamed the graphs

--- make_hist.py
+++ make_hist.py
@@ -9,40 +9,40 @@ ratings_array, rating_info = ut.get_categories()
 plt.hist(ratings_array, bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of All Ratings')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.1.png')
+plt.savefig('histograms/41.png')
 plt.show()
 
 # Generate histogram of ratings of 10 best movies
 plt.hist(rating_info[0], bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of Ratings of 10 Best Movies')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.3.png')
+plt.savefig('histograms/43.png')
 plt.show()
 
 # Generate histogram of ratings of 10 most popular movies
 plt.hist(rating_info[1], bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of Ratings of 10 Most Popular Movies')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.2.png')
+plt.savefig('histograms/42.png')
 plt.show()
 
 # Generate histogram of ratings of Animation movies
 plt.hist(rating_info[2], bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of Ratings of Animation Movies')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.4anim.png')
+plt.savefig('histograms/44anim.png')
 plt.show()
 
 # Generate histogram of ratings of Comedy movies
 plt.hist(rating_info[3], bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of Ratings of Comedy Movies')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.4com.png')
+plt.savefig('histograms/44com.png')
 plt.show()
 
 # Generate histogram of ratings of Musical movies
 plt.hist(rating_info[4], bins=np.arange(0.5, 6.5, 1))
 plt.title('Histogram of Ratings of Musical Movies')
 plt.xlabel('Ratings')
-plt.savefig('histograms/4.4mus.png')
+plt.savefig('histograms/44mus.png')
 plt.show()
