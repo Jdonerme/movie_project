ed56de9d26a876f73e6081934b5c7f4e32f94bee
mortonrj <morton.rjt@gmail.com>
2017-03-09 16:21:59 -0800

    Made basic graphs

--- matrix_visualizations.py
+++ matrix_visualizations.py
@@ -18,8 +18,7 @@ def create_u_v_matrices():
     eps = 0.01
     max_epochs = 100
 
-    result = HW5utils.train_model(M, N, K, eta, reg, Y, eps, max_epochs)
-    U, V, error = result
+    U, V, error = HW5utils.train_model(M, N, K, eta, reg, Y, eps, max_epochs)
 
     np.savetxt('U_matrix.txt', U, delimiter=',')
     np.savetxt('V_matrix.txt', V, delimiter=',')
@@ -34,12 +33,8 @@ def apply_svd():
     U_tilde = np.dot(A[:, [0, 1]].transpose(), V)
     return V_tilde, U_tilde
 
-def part_a_visual():
+def make_visual(movies, title='Matrix Visualization'):
     V_tilde, U_tilde = apply_svd()
-
-    get_top_ten_movies = function()
-    # Rescale dimensions
-
 
     # Plot projected  V
     print(len(V_tilde))
@@ -48,19 +43,43 @@ def part_a_visual():
 
     x = []
     y = []
+    print(len(movies))
+    for movie in movies:
+        print(movie)
 
-    for movie in top_ten_movies:
-        new_x = V_tilde[0]:
-        new_y = V_tile[1]
+        new_x = V_tilde[0][movie]
+        new_y = V_tilde[1][movie]
 
         x.append(new_x)
         y.append(new_y)
 
+    plt.title(title)
+    plt.xlabel('X') # Should probably be a better name
+    plt.ylabel('Y') # Same
+    plt.scatter(x, y)
+    plt.show()
 
-    #plt.scatter()
-    #plt.show()
 
+def main():
+    movies, ids, titles = ut.get_category_ids(genres_desired=['Animation', 'Comedy', 'Musical'])
+    top, pop, g1, g2, g3 = ids
 
+    # Part A
+    make_visual(movies, title='Ten Random Movies Matrix Factorization Visualization')
 
+    # Part B
+    make_visual(pop, title='Ten Most Popular Movies Matrix Factorization Visualization')
 
-part_a_visual()
+    # Part C
+    make_visual(top, title='Ten Best Movies Matrix Factorization Visualization')
+
+    # Part D, genre 1
+    make_visual(g1, title='Ten Animation Movies Matrix Factorization Visualization')
+
+    # Part D, genre 2
+    make_visual(g2, title='Ten Comedy Movies Matrix Factorization Visualization')
+
+    # Part D, genre 3
+    make_visual(g3, title='Ten Musical Movies Matrix Factorization Visualization')
+
+main()
